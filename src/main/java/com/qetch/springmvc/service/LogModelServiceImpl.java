a68c4c17fbe4be9e5866d23a12375577aa85e8b4
JiaFeiCat93 <37857620+JiaFeiCat93@users.noreply.github.com>
2019-03-12 19:56:14 +0800

    try update ah 20190312

--- src/main/java/com/qetch/springmvc/service/LogModelServiceImpl.java
+++ src/main/java/com/qetch/springmvc/service/LogModelServiceImpl.java
@@ -1,14 +1,17 @@
 package com.qetch.springmvc.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.qetch.springmvc.domain.LogModel;
 
 @Service
 public class LogModelServiceImpl implements LogModelService {
+	private static final Logger logger = LoggerFactory.getLogger(LogModelServiceImpl.class);
 
 	@Override
 	public void saveLogModel(LogModel logModel) {
-		
+		logger.info("正在保存..." + logModel.toString());
 	}
 }
